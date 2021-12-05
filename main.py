0b42c8452226df9f073e9b0602775e0a41562bb3
TGZolars <70763346+TGZolars@users.noreply.github.com>
2021-12-05 22:44:11 +0800

    numpy ver 2.0
    Six times faster than clear python!

--- main.py
+++ main.py
@@ -6,66 +6,37 @@ import sys
 
 def compute_neighbours(Z):
     start_time = pygame.time.get_ticks()
-    shape = Z.shape
-    N = np.asarray([[0 for i in range(shape[1])] for i in range(shape[0])])
-    
-    # for x in range(1, shape[0]-1):
-    #     for y in range(1, shape[1]-1):
-    #         N[x][y] += Z[x - 1][y - 1] + Z[x][y - 1] + Z[x + 1][y - 1] \
-    #                 + Z[x - 1][y    ]               + Z[x + 1][y    ] \
-    #                 + Z[x - 1][y + 1] + Z[x][y + 1] + Z[x + 1][y + 1]
 
     N = np.zeros_like(Z)
     N[1:-1,1:-1] = Z[:-2,:-2] + Z[1:-1,:-2] + Z[:-2,1:-1] \
                  + Z[2:,:-2]                + Z[2:,1:-1] \
                  + Z[:-2,2:]  + Z[1:-1,2:]  + Z[2:,2:]
-    
+
     print("compute neighbours ", (pygame.time.get_ticks() - start_time)/1000)
     return N
 
 
-def iterate(Z):
-    shape = Z.shape
-    N = compute_neighbours(Z)
-    
-    start_time = pygame.time.get_ticks()
-    
-    for x in range(1, shape[0] - 1):
-        for y in range(1, shape[1] - 1):
-            if (Z[x][y] and (N[x][y] < 2 or N[x][y] > 3)) or (not Z[x][y] and N[x][y] == 3):
-                Z[x][y] = not Z[x][y]
-    
-    print("iterate ", (pygame.time.get_ticks() - start_time)/1000)
+def iterate(Z, N):
+    if (Z and (N < 2 or N > 3)) or (not Z and N == 3):
+        Z = not Z
+
     return Z
 
 
 def draw_blocks(screen, xlen, ylen, world, alive_color, block_size):
     start_time = pygame.time.get_ticks()
     for xx in range(xlen):
-            for yy in range(ylen):
-                alive = world[xx][yy]
-                if alive:
-                    x = (xx - 1) * block_size
-                    y = (yy - 1) * block_size
-                    pygame.draw.rect(screen, alive_color, ((x, y), (block_size, block_size)))
+        for yy in range(ylen):
+            alive = world[xx][yy]
+            if alive:
+                x = (xx - 1) * block_size
+                y = (yy - 1) * block_size
+                pygame.draw.rect(screen, alive_color, ((x, y), (block_size, block_size)))
 
     print("draw ", (pygame.time.get_ticks() - start_time)/1000)
 
 
 def make_random_grid(x, y):
-    # grid = np.asarray([[0 for i in range(y)]])
-
-    # for r in range(x-2):
-    #     row = [0]
-    
-    #     for c in range(y-2):
-    #         row.append(randint(0, 1))
-    
-    #     row.append(0)
-    #     grid.append(row)
-    
-    # grid.append([0 for i in range(y)])
-
     grid = np.random.randint(0, 2, size=(x, y))
     grid[:, 0] *= 0
     grid[:, -1] *= 0
@@ -88,6 +59,8 @@ def main():
     xlen = xmax // scale + 2
     ylen = ymax // scale + 2
     world = make_random_grid(xlen, ylen)
+
+    viterate = np.vectorize(iterate)
     
     while True:
         screen.fill("black")
@@ -104,7 +77,11 @@ def main():
         
         # screen.blit(surf, (0, 0))
 
-        world = iterate(world)
+        N = compute_neighbours(world)
+
+        start_time = pygame.time.get_ticks()
+        world = viterate(world, N)
+        print("iterate ", (pygame.time.get_ticks() - start_time)/1000)
 
         pygame.display.set_caption("$~GoL ~fps: " + str(round(clock.get_fps(), 2)))
 
