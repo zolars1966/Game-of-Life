fd6ad034616d6563c42b32467a1f8fa7bce415e3
TGZolars <70763346+TGZolars@users.noreply.github.com>
2021-12-05 22:10:47 +0800

    numpy ver 1.0
    Three times faster than clear python!

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 from random import randint
+import numpy as np
 import pygame
 import sys
 
 
 def compute_neighbours(Z):
     start_time = pygame.time.get_ticks()
-    shape = len(Z), len(Z[0])
-    N = [[0 for i in range(shape[1])] for i in range(shape[0])]
+    shape = Z.shape
+    N = np.asarray([[0 for i in range(shape[1])] for i in range(shape[0])])
     
-    for x in range(1, shape[0]-1):
-        for y in range(1, shape[1]-1):
-            N[x][y] = Z[x - 1][y - 1] + Z[x][y - 1] + Z[x + 1][y - 1] \
-                    + Z[x - 1][y    ]               + Z[x + 1][y    ] \
-                    + Z[x - 1][y + 1] + Z[x][y + 1] + Z[x + 1][y + 1]
+    # for x in range(1, shape[0]-1):
+    #     for y in range(1, shape[1]-1):
+    #         N[x][y] += Z[x - 1][y - 1] + Z[x][y - 1] + Z[x + 1][y - 1] \
+    #                 + Z[x - 1][y    ]               + Z[x + 1][y    ] \
+    #                 + Z[x - 1][y + 1] + Z[x][y + 1] + Z[x + 1][y + 1]
 
+    N = np.zeros_like(Z)
+    N[1:-1,1:-1] = Z[:-2,:-2] + Z[1:-1,:-2] + Z[:-2,1:-1] \
+                 + Z[2:,:-2]                + Z[2:,1:-1] \
+                 + Z[:-2,2:]  + Z[1:-1,2:]  + Z[2:,2:]
+    
     print("compute neighbours ", (pygame.time.get_ticks() - start_time)/1000)
     return N
 
 
 def iterate(Z):
-    start_time = pygame.time.get_ticks()
-    shape = len(Z), len(Z[0])
+    shape = Z.shape
     N = compute_neighbours(Z)
     
-    for x in range(1, shape[0]-1):
-        for y in range(1, shape[1]-1):
-            if Z[x][y] and (N[x][y] < 2 or N[x][y] > 3):
-                Z[x][y] = not Z[x][y]
-            elif not Z[x][y] and N[x][y] == 3:
+    start_time = pygame.time.get_ticks()
+    
+    for x in range(1, shape[0] - 1):
+        for y in range(1, shape[1] - 1):
+            if (Z[x][y] and (N[x][y] < 2 or N[x][y] > 3)) or (not Z[x][y] and N[x][y] == 3):
                 Z[x][y] = not Z[x][y]
     
     print("iterate ", (pygame.time.get_ticks() - start_time)/1000)
@@ -40,27 +45,32 @@ def draw_blocks(screen, xlen, ylen, world, alive_color, block_size):
             for yy in range(ylen):
                 alive = world[xx][yy]
                 if alive:
-                    x = xx * block_size
-                    y = yy * block_size
-                    center_point = ((x + (block_size / 2)), (y + (block_size / 2)))
+                    x = (xx - 1) * block_size
+                    y = (yy - 1) * block_size
                     pygame.draw.rect(screen, alive_color, ((x, y), (block_size, block_size)))
 
     print("draw ", (pygame.time.get_ticks() - start_time)/1000)
 
 
 def make_random_grid(x, y):
-    grid = [[0 for i in range(y)]]
+    # grid = np.asarray([[0 for i in range(y)]])
 
-    for r in range(x-2):
-        row = [0]
+    # for r in range(x-2):
+    #     row = [0]
     
-        for c in range(y-2):
-            row.append(randint(0, 1))
+    #     for c in range(y-2):
+    #         row.append(randint(0, 1))
     
-        row.append(0)
-        grid.append(row)
+    #     row.append(0)
+    #     grid.append(row)
     
-    grid.append([0 for i in range(y)])
+    # grid.append([0 for i in range(y)])
+
+    grid = np.random.randint(0, 2, size=(x, y))
+    grid[:, 0] *= 0
+    grid[:, -1] *= 0
+    grid[0] *= 0
+    grid[-1] *= 0
     
     return grid
 
@@ -68,8 +78,6 @@ def make_random_grid(x, y):
 def main():
     xmax = int(sys.argv[1])
     ymax = int(sys.argv[2])
-
-    fps = int(sys.argv[4])
 
     screen = pygame.display.set_mode((xmax, ymax))
     clock = pygame.time.Clock()
@@ -83,6 +91,9 @@ def main():
     
     while True:
         screen.fill("black")
+
+        # surf = pygame.surfarray.make_surface(world)
+        # surf = pygame.transform.scale(surf, (xlen * scale, ylen * scale))
         
         keys = pygame.key.get_pressed()
         for event in pygame.event.get():
@@ -91,13 +102,14 @@ def main():
 
         draw_blocks(screen, xlen, ylen, world, alive_color, scale)
         
+        # screen.blit(surf, (0, 0))
+
         world = iterate(world)
 
         pygame.display.set_caption("$~GoL ~fps: " + str(round(clock.get_fps(), 2)))
 
         pygame.display.flip()
-        if not fps:
-            clock.tick(fps)
+        clock.tick()
 
 if __name__ == '__main__':
     main()
