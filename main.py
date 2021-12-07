4f91aadc894d9fc96bb0cd516d4b32b07f864841
TGZolars <70763346+TGZolars@users.noreply.github.com>
2021-12-07 16:08:23 +0800

    numpy ver 3.0
    52 times faster than clear python!

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ def compute_neighbours(Z):
 
 
 def iterate(Z, N):
-    if (Z and (N < 2 or N > 3)) or (not Z and N == 3):
-        Z = not Z
-
-    return Z
-
-
-def draw_blocks(screen, xlen, ylen, world, alive_color, block_size):
     start_time = pygame.time.get_ticks()
-    for xx in range(xlen):
-        for yy in range(ylen):
-            alive = world[xx][yy]
-            if alive:
-                x = (xx - 1) * block_size
-                y = (yy - 1) * block_size
-                pygame.draw.rect(screen, alive_color, ((x, y), (block_size, block_size)))
 
+    Z[np.where(N > 3)] = 0
+    Z[np.where(N < 2)] = 0
+    Z[np.where(N == 3)] = 1
+    
+    print("iterate ", (pygame.time.get_ticks() - start_time)/1000)
+
+
+def draw_blocks(screen, xlen, ylen, world, block_size, color):
+    start_time = pygame.time.get_ticks()
+
+    surf = pygame.surfarray.make_surface(world * color)
+    
+    screen.blit(surf, (-1, -1))
+    
     print("draw ", (pygame.time.get_ticks() - start_time)/1000)
 
 
@@ -50,40 +50,37 @@ def main():
     xmax = int(sys.argv[1])
     ymax = int(sys.argv[2])
 
-    screen = pygame.display.set_mode((xmax, ymax))
+    screen = pygame.display.set_mode((xmax, ymax), pygame.SCALED)
     clock = pygame.time.Clock()
 
     h = 0
-    alive_color = pygame.Color(0, 255, 0)
-    scale = int(sys.argv[3])
+    scale = 1
     xlen = xmax // scale + 2
     ylen = ymax // scale + 2
     world = make_random_grid(xlen, ylen)
+    color = 1
 
     viterate = np.vectorize(iterate)
     
     while True:
         screen.fill("black")
 
-        # surf = pygame.surfarray.make_surface(world)
-        # surf = pygame.transform.scale(surf, (xlen * scale, ylen * scale))
-        
         keys = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 0
 
-        draw_blocks(screen, xlen, ylen, world, alive_color, scale)
+        draw_blocks(screen, xlen, ylen, world, scale, color)
         
         # screen.blit(surf, (0, 0))
 
         N = compute_neighbours(world)
 
-        start_time = pygame.time.get_ticks()
-        world = viterate(world, N)
-        print("iterate ", (pygame.time.get_ticks() - start_time)/1000)
+        iterate(world, N)
 
         pygame.display.set_caption("$~GoL ~fps: " + str(round(clock.get_fps(), 2)))
+
+        color += 1
 
         pygame.display.flip()
         clock.tick()
